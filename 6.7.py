c0dd648b22ee0964d850c29425d142ef40b43583
Dominik <dkow.2204@gmail.com>
2024-08-30 00:21:27 +0200

    lekcje z dnia 29.08.2024

---
+++ 6.7.py
@@ -0,0 +1,25 @@
+marek = {
+    'first_name': 'marek', 
+    'last_name': 'spoko', 
+    'age': 24, 
+    'city': 'rybnik',
+}
+
+samuel = {
+    'first_name': 'samuel',
+    'last_name': 'gites',
+    'age': 22,
+    'city': 'wrocław',
+}
+
+dawid = {
+    'first_name': 'dawid',
+    'last_name': 'kozioł',
+    'age': 23,
+    'city': 'garwolin',
+}
+
+people = [marek, samuel, dawid]
+
+for person in people:
+    print(person)
---
+++ 6.8.py
@@ -0,0 +1,28 @@
+franek = {
+    'type': 'cat',
+    'color': 'black',
+    'name': 'franek',
+}
+
+gangsterek = {
+    'type': 'cat',
+    'color': 'black',
+    'name': 'gangsterek',
+}
+
+jadzia = {
+    'type': 'cat',
+    'color': 'white',
+    'name': 'jadwiga',
+}
+
+puma = {
+    'type': 'cat',
+    'color': 'black',
+    'name': 'puma',
+}
+
+pets = [franek, gangsterek, jadzia, puma]
+
+for pet in pets:
+    print(pet)
