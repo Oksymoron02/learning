deb9b1063b5cd9fba557f769d3a084d2f1a00e20
Dominik <dkow.2204@gmail.com>
2025-02-10 00:36:58 +0100

    9.02.2025

--- pizza.py
+++ pizza.py
@@ -2,6 +2,3 @@ def make_pizza(size, *toppings):
     print(f"\nPrzygotowuję pizzę o wielkości {str(size)} cm, z następującymi dodatkami: ")
     for topping in toppings:
         print(f"- {topping}")
-
-make_pizza(40, 'pepperoni')
-make_pizza(30, 'pieczarki', 'zielona papryka', 'podwójny ser')
